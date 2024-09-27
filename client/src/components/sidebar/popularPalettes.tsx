6420e3c573903f7de9b421e004440c3c8a922141
ManChuenLee <78773855+ManChuenLee@users.noreply.github.com>
2024-09-27 04:01:11 -0700

    Uncommented popular palettes

--- client/src/components/sidebar/popularPalettes.tsx
+++ client/src/components/sidebar/popularPalettes.tsx
@@ -1,23 +1,23 @@
-import { useContext } from "react"
-import ColorsContext from "../../context/colorsContextProvider"
+// import { useContext } from "react"
+// import ColorsContext from "../../context/colorsContextProvider"
 import '../../scss/components/sidebar/popularPalettes.scss'
-import { color } from "../../types/colors"
+// import { color } from "../../types/colors"
 
 const PopularPalettes = () => {
-    const {colors} = useContext(ColorsContext)
+    // const {colors} = useContext(ColorsContext)
 
-    function createGradient(colors: color[], direction: string){
-        let percentage = (1 / colors.length) * 100
-        let style = `linear-gradient(to ${direction},`
-        for(let i = 0; i < colors.length; i++){
-            if(i !== colors.length - 1){
-                style += `${colors[i].color} ${percentage * i}% ${percentage * (i + 1)}%,`
-            } else {
-                style += `${colors[i].color} ${percentage * i}% ${percentage * (i + 1)}%)`
-            }
-        }
-        return style
-    }
+    // function createGradient(colors: color[], direction: string){
+    //     let percentage = (1 / colors.length) * 100
+    //     let style = `linear-gradient(to ${direction},`
+    //     for(let i = 0; i < colors.length; i++){
+    //         if(i !== colors.length - 1){
+    //             style += `${colors[i].color} ${percentage * i}% ${percentage * (i + 1)}%,`
+    //         } else {
+    //             style += `${colors[i].color} ${percentage * i}% ${percentage * (i + 1)}%)`
+    //         }
+    //     }
+    //     return style
+    // }
 
     return(
         <section>

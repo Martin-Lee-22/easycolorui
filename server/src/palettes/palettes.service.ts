65cb25e5dc24aa16d24f6a5a9b71ceeab97e19c8
ManChuenLee <78773855+ManChuenLee@users.noreply.github.com>
2024-10-02 12:45:35 -0700

    Cleanred up palette service

--- server/src/palettes/palettes.service.ts
+++ server/src/palettes/palettes.service.ts
@@ -1,7 +1,4 @@
-import { Injectable, NotAcceptableException } from '@nestjs/common';
-import { palette } from 'src/types';
-import { checkForDuplicates, checkForEmptyClasses } from 'src/util/helperFunctions';
-import { CreatePaletteDto } from './dto/create-palette.dto';
+import { Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { DatabaseService } from 'src/database/database.service';
 
@@ -10,6 +7,7 @@ export class PalettesService {
     constructor(private readonly databaseService: DatabaseService){}
 
     async getPalettes(){
+        console.log('Retrieving palette from database...')
         return this.databaseService.palette.findMany({
             include: {
                 colors: true,
@@ -18,19 +16,6 @@ export class PalettesService {
     }
 
     async createPalettes(palette: Prisma.PaletteCreateInput){
-        // if(checkForEmptyClasses(palette)){ throw new NotAcceptableException('Cannot Upload Palette', 
-        //     { cause: new Error(), description:'All colors in your palette must have colored at least one element.'})}
-        
-        // if(checkForDuplicates(palette, this.palettes)){ throw new NotAcceptableException('Cannot Upload Palette', {
-        //     cause: new Error(), description:'There is an already existing palette with the same colors.'})}
-
-        // const palettesByHighestId = this.palettes.sort((a, b) => b.id - a.id)
-        // const newPalette = {
-        //     id: palettesByHighestId[0].id + 1,
-        //     ...palette
-        // }
-        // this.palettes.push(newPalette)
-        console.log(palette)
         const newPalette = {
             colors:{
                 create: palette.colors as Prisma.ColorsCreateWithoutPaletteInput[]

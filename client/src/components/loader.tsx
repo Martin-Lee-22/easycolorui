faff0949aeea28d0c40817738bdea9e818a128b7
ManChuenLee <78773855+ManChuenLee@users.noreply.github.com>
2024-11-04 16:00:43 -0800

    Added reason for slow loading

--- client/src/components/loader.tsx
+++ client/src/components/loader.tsx
@@ -1,7 +1,10 @@
 import '../scss/components/loader.scss'
 
 const Loader = () => {
-    return <span className="loader"></span>
+    return <div className='loading-container'>
+        <span className="loader"/>
+        <p>Reinitializing server: may take a few minutes due to using Neon's free servers tier. Please don't leave <span>&#128557;</span></p>
+        </div>
 }
 
 export default Loader
